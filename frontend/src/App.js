1758219e21a4ce2ab46a4bc61405e14b04599456
Andrey Samoryadov <samoryad@mail.ru>
2021-09-21 00:44:58 +0300

    users rendered by dynamic method from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from "react";
 import UserList from "./components/Users";
+import axios from "axios";
 
 class App extends React.Component {
     constructor(props) {
@@ -11,25 +12,16 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        const users =[
-            {
-        "username": "",
-        "first_name": "Ashot",
-        "last_name": "Potikyan",
-        "email": "Ashot@gb.ru"
-        },
-            {
-        "username": "django",
-        "first_name": "",
-        "last_name": "",
-        "email": "django@gb.ru"
-        }
-        ]
-        this.setState(
-            {
-                'users': users
-            }
-        )
+        axios.get('http://127.0.0.1:8000/api/users/')
+            .then(response => {
+                const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
+            })
+            .catch(error => console.log(error))
     }
 
     render() {
@@ -40,27 +32,5 @@ class App extends React.Component {
         )
     }
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
