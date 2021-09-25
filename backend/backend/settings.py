f1c6368bebfd1416d037a28610acfcada398550e
Andrey Samoryadov <samoryad@mail.ru>
2021-09-25 15:17:48 +0300

    homework_03 (renderers added to settings, TODO task with *)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,6 +44,14 @@ INSTALLED_APPS = [
     'todoapp',
 ]
 
+# настройки рендереров для rest
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000'
