8fe52d6592f68af303c0884cf83651f472a0fe5f
Andrey Samoryadov <samoryad@mail.ru>
2021-09-25 15:38:28 +0300

    homework_03 done (camel_case was hidden in rest_framework renderers settings)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,6 +49,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        # 'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
     ]
 }
 
