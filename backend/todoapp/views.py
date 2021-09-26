486f6863dd3449b17cf63eaf49fe763c98a8d248
Andrey Samoryadov <samoryad@mail.ru>
2021-09-27 00:32:39 +0300

    homework_04 started (3.2, 3.3 task done - only pagination))

--- backend/todoapp/views.py
+++ backend/todoapp/views.py
@@ -1,13 +1,24 @@
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDo
 from .serializers import ProjectSerializer, ToDoSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
 class ProjectModelViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+    pagination_class = ProjectLimitOffsetPagination
 
 
 class ToDoModelViewSet(ModelViewSet):
     serializer_class = ToDoSerializer
     queryset = ToDo.objects.all()
+    pagination_class = ToDoLimitOffsetPagination
