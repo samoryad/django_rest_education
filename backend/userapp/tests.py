8a660c8cf26e97b0575cd36a70cf83270715fa9f
Andrey Samoryadov <samoryad@mail.ru>
2021-10-06 01:46:08 +0300

    homework_06 done (authentication by token checked)

--- backend/userapp/tests.py
+++ backend/userapp/tests.py
@@ -8,7 +8,7 @@ response = requests.post(
     'http://127.0.0.1:8000/api-token-auth/',
     data={
         'username': 'django1',
-        'password': 'йцукен12345'})
+        'password': 'Zaq11qaz'})
 
 # {'token': '33dd9e2d633aa75d6b53b6a47346a404ed165693'}
 print(response.status_code)
