313a176e50000bcbb472a459c426c471992134ea
Andrey Samoryadov <samoryad@mail.ru>
2021-10-06 00:46:33 +0300

    homework_06 done (authentication by token checked)

--- backend/userapp/management/commands/create_user.py
+++ backend/userapp/management/commands/create_user.py
@@ -4,7 +4,10 @@ from userapp.models import ToDoUser
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        ToDoUser.objects.create(first_name='Ashot', email="Ashot@gb.ru",
-                                last_name='Potikyan')
+        ToDoUser.objects.create(
+            username='Ashot',
+            first_name='Ashot',
+            email="Ashot@gb.ru",
+            last_name='Potikyan')
         ToDoUser.objects.create_superuser(
             'django', "django@gb.ru", 'geekbrains')
