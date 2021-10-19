bbc1a6b76b7c3ed1d76b5c58f25e3c6b5e0f7ab0
Andrey Samoryadov <samoryad@mail.ru>
2021-10-20 00:34:58 +0300

    homework_10 (all_users, all_projects, all_notices, user_by_id, notices_by_username done)

--- backend/userapp/schema.py
+++ backend/userapp/schema.py
@@ -1,8 +1,57 @@
 import graphene
+from graphene_django import DjangoObjectType
+
+from todoapp.models import ToDo, Project
+from userapp.models import ToDoUser
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = ToDoUser
+        fields = '__all__'
+
+
+class ProjectType(DjangoObjectType):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class ToDoType(DjangoObjectType):
+    class Meta:
+        model = ToDo
+        fields = '__all__'
 
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    all_users = graphene.List(UserType)
+    all_projects = graphene.List(ProjectType)
+    all_notices = graphene.List(ToDoType)
+    user_by_id = graphene.Field(UserType, id=graphene.Int(required=True))
+    notices_by_username = graphene.List(
+        ToDoType, name=graphene.String(
+            required=False))
+
+    def resolve_all_users(self, info):
+        return ToDoUser.objects.all()
+
+    def resolve_all_projects(self, info):
+        return Project.objects.all()
+
+    def resolve_all_notices(self, info):
+        return ToDo.objects.all()
+
+    def resolve_user_by_id(self, info, id):
+        try:
+            return ToDoUser.objects.get(pk=id)
+        except ToDoUser.DoesNotExist:
+            return None
+
+    def resolve_notices_by_username(self, info, name=None):
+        notices = ToDo.objects.all()
+        if name:
+            notices = notices.filter(user__username=name)
+        return notices
 
 
 schema = graphene.Schema(query=Query)
